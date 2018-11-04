be5fb3845ed39eb166ec494b27ff6af1e894f765
rrrjuchin <37605611+rrrjuchin@users.noreply.github.com>
2018-11-04 21:55:07 +0900

    Add files via upload

---
+++ visualization.py
@@ -0,0 +1,30 @@
+import os
+
+def write_vis(filename, upcode_str, dncode_str) :
+    name = filename + ".ly"
+    versionlp = '\\version "2.18.2"\n'
+    upcode_st = "up=\\drummode {\n"
+    upcode_ed = "}\n"
+    dncode_st = "down=\\drummode {\n"
+    dncode_ed = "}\n"
+    basic_notation1 = "\\new DrumStaff<<\n"
+    #basic_notation2 = "new DrumVoice {\\voiceOne \up}"
+    basic_notation2 = " \\new DrumVoice {\\voiceTwo \\up}\n"
+    basic_notation3 = " \\new DrumVoice {\\voiceTwo \\down}\n>>"
+    basic_notation = basic_notation1+ basic_notation2 + basic_notation3
+    vis = open(name,'w')
+    vis.write(versionlp)
+    vis.write(upcode_st)
+    vis.write(upcode_ed)
+    vis.write(dncode_st)
+    vis.write(dncode_ed)
+    vis.write(basic_notation)
+    vis.close()
+
+def read_vis(filename) :
+    vis = open(filename,'r')
+
+def produce_pdf(filename) :
+    os.system("lilypond result.ly")
+
+write_vis("result1")
